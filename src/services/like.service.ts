8c166c32070d1e39a87990d14682ee934a8f7e15
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-10-07 14:52:05 +0300

    sync like functionality with frontend

--- src/services/like.service.ts
+++ src/services/like.service.ts
@@ -1,9 +1,11 @@
 import { IComment, ILike } from "../typescript/interfaces";
 import Like from "../db/models/Like";
+import HttpError from "../typescript/classes/HttpError";
 
 export const createLike = async (like: ILike): Promise<ILike> => {
-  const createdLike: Like = await Like.create({
-    ...like,
+  const [createdLike, isCreated]: [Like, boolean] = await Like.findOrCreate({
+    where: { ...like },
   });
+  if (!isCreated) throw new HttpError(409, "The post has already been liked");
   return createdLike.toJSON();
 };
