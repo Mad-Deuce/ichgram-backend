b450c7108fcad568bb8e4fe19eaeb994e34ba2f9
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-10-08 21:07:22 +0300

    changed getLastUpdatedPosts function

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,3 +1,4 @@
+import { Op } from "sequelize";
 import Notification from "../db/models/Notification";
 import { INotification } from "../typescript/interfaces";
 
@@ -6,4 +7,33 @@ export const createNotification = async (
 ): Promise<INotification> => {
   const result: Notification = await Notification.create({ ...payload });
   return result.toJSON();
+};
+
+export const getLastUpdates = async (
+  userId: number
+): Promise<INotification[]> => {
+  const personalNotificationModels: Notification[] = await Notification.findAll(
+    {
+      where: {
+        targetUserId: userId,
+      },
+      limit: 10,
+    }
+  );
+
+  const personalNotifications: INotification[] = personalNotificationModels.map(
+    (notification) => notification.toJSON()
+  );
+  const resultLength = personalNotifications.length;
+  if (resultLength > 9) return personalNotifications;
+  const otherNotificationModels: Notification[] = await Notification.findAll({
+    where: {
+      targetUserId: { [Op.not]: userId },
+    },
+    limit: 10 - resultLength,
+  });
+  const otherNotifications: INotification[] = otherNotificationModels.map(
+    (notification) => notification.toJSON()
+  );
+  return personalNotifications.concat(otherNotifications);
 };
--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -3,6 +3,7 @@ import { Op } from "sequelize";
 import {
   IComment,
   ILike,
+  INotification,
   IPost,
   IPostResponse,
   IUser,
@@ -17,6 +18,7 @@ import Follow from "../db/models/Follow";
 import { getCommentsByPostIds } from "./comment.service";
 import { getLikesCount, isPostsLiked } from "./like.service";
 import HttpError from "../typescript/classes/HttpError";
+import { getLastUpdates } from "./notification.service";
 
 export const createPost = async (post: IPost): Promise<IPost> => {
   const createdPost: Post = await Post.create({
@@ -25,7 +27,122 @@ export const createPost = async (post: IPost): Promise<IPost> => {
   return createdPost.toJSON();
 };
 
+const getLastUpdatedPostIds = async (userId: number): Promise<number[]> => {
+  const lastUpdates: INotification[] = await getLastUpdates(userId);
+  const lastUpdatedPostIds: number[] = lastUpdates
+    .map((notification) => notification.targetPostId)
+    .filter((id): id is number => id !== null);
+  const lastUpdatedPostIdsSet: number[] = [...new Set(lastUpdatedPostIds)];
+
+  const resultLength = lastUpdatedPostIdsSet.length;
+  if (resultLength > 9) return lastUpdatedPostIds;
+
+  const postModels: Post[] = await Post.findAll({
+    limit: 10 - resultLength,
+    order: [["updatedAt", "DESC"]],
+    where: {
+      id: {
+        [Op.notIn]: lastUpdatedPostIds,
+      },
+    },
+    attributes: ["id"],
+  });
+  const postIds = postModels.map((post) => post.toJSON().id);
+
+  const postIdsSet: number[] = [...new Set(lastUpdatedPostIds.concat(postIds))];
+  return postIdsSet;
+};
+
+export const getPostsByIds = async (
+  userId: number,
+  postIds: number[]
+): Promise<(IPost & { user?: IUser })[]> => {
+  const postModels: (Post & { user?: User })[] = await Post.findAll({
+    order: [["updatedAt", "DESC"]],
+    where: {
+      id: {
+        [Op.in]: postIds,
+      },
+    },
+    include: [
+      {
+        model: User,
+        as: "user",
+        attributes: { exclude: ["password", "role", "isVerified"] },
+        include: [
+          {
+            model: Follow,
+            as: "followers",
+            where: { followerUserId: userId },
+            required: false,
+          },
+        ],
+      },
+    ],
+  });
+  const result: (IPost & { user?: IUser })[] = postModels.map((post) =>
+    post.toJSON()
+  );
+  return result;
+};
+
+const getDetailedPosts = async (
+  userId: number,
+  posts: IPost[]
+): Promise<any> => {
+  const postIds: number[] = posts.map((post) => post.id);
+
+  const comments: (IComment & { user?: IUser })[] = await getCommentsByPostIds(
+    postIds
+  );
+
+  posts.forEach((post) => {
+    comments.forEach((comment) => {
+      if (post.id === comment.postId) {
+        if (!post.comments) post.comments = [];
+        if (!post.totalComments) post.totalComments = 0;
+        post.comments.push(comment);
+        post.totalComments += 1;
+      }
+    });
+    if (Array.isArray(post.comments)) {
+      post.comments = post.comments
+        .sort((a, b) => (b.updatedAt as any) - (a.updatedAt as any))
+        .slice(0, 4);
+    }
+  });
+
+  const likes: (ILike & { count?: number })[] = await getLikesCount(postIds);
+  const isLiked: ILike[] = await isPostsLiked(postIds, userId);
+
+  posts.forEach((post) => {
+    likes.forEach((like) => {
+      if (post.id === like.postId) {
+        post.totalLikes = like.count;
+      }
+    });
+
+    isLiked.forEach((like) => {
+      if (post.id === like.postId) {
+        post.isLiked = true;
+      }
+    });
+  });
+
+  return posts;
+};
+
 export const getLastUpdatedPosts = async (userId: number): Promise<any> => {
+  const lastUpdatedPostIds: number[] = await getLastUpdatedPostIds(userId);
+  const posts = await getPostsByIds(userId, lastUpdatedPostIds);
+  const detailedPosts = getDetailedPosts(userId, posts);
+  return detailedPosts;
+};
+
+/**
+ * @deprecated The method should not be used
+ */
+export const getLastUpdatedPostsAlt2 = async (userId: number): Promise<any> => {
   const postModels: Post[] = await Post.findAll({
     limit: 4,
     order: [["updatedAt", "DESC"]],
@@ -90,6 +207,9 @@ export const getLastUpdatedPosts = async (userId: number): Promise<any> => {
   return posts;
 };
 
+/**
+ * @deprecated The method should not be used
+ */
 export const getLastUpdatedPostsAlt = async (
   userId: number
 ): Promise<IPost[]> => {
